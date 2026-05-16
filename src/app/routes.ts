50664386931106c6de0889ffc9aa8213c23b884c
dnlvn <dan@appero.se>
2026-05-16 12:09:29 +0200

    Add electricity comparison MVP

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router";
 import { Layout } from "@/app/components/Layout";
 import { LandingPage } from "@/app/pages/LandingPage";
 import { MobileComparison } from "@/app/pages/MobileComparison";
+import { ElectricityComparison } from "@/app/pages/ElectricityComparison";
 import { Integritetspolicy } from "@/app/pages/Integritetspolicy";
 import { Cookies } from "@/app/pages/Cookies";
 
@@ -17,6 +18,10 @@ export const router = createBrowserRouter([
       {
         path: "mobilabonnemang",
         Component: MobileComparison,
+      },
+      {
+        path: "elavtal",
+        Component: ElectricityComparison,
       },
       {
         path: "integritetspolicy",
