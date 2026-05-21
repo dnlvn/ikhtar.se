5d48884023ba365ed843ddde525d45d975356691
dnlvn <dan@appero.se>
2026-05-21 21:39:37 +0200

    Register mobile SEO routes

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,7 +2,18 @@ import { createBrowserRouter } from "react-router";
 import { Layout } from "@/app/components/Layout";
 import { LandingPage } from "@/app/pages/LandingPage";
 import { MobileComparison } from "@/app/pages/MobileComparison";
-import { CheapestMobileSubscriptionPage } from "@/app/pages/MobileSeoPage";
+import {
+  CheapestMobileSubscriptionPage,
+  ComviqMobileSeoPage,
+  FelloMobileSeoPage,
+  NoBindingMobileSeoPage,
+  SurfGuideMobileSeoPage,
+  Tele2MobileSeoPage,
+  TelenorMobileSeoPage,
+  TeliaMobileSeoPage,
+  TreMobileSeoPage,
+  VimlaMobileSeoPage,
+} from "@/app/pages/MobileSeoPage";
 import { ElectricityComparison } from "@/app/pages/ElectricityComparison";
 import { Integritetspolicy } from "@/app/pages/Integritetspolicy";
 import { Cookies } from "@/app/pages/Cookies";
@@ -23,6 +34,42 @@ export const router = createBrowserRouter([
       {
         path: "mobilabonnemang/billigaste",
         Component: CheapestMobileSubscriptionPage,
+      },
+      {
+        path: "mobilabonnemang/comviq",
+        Component: ComviqMobileSeoPage,
+      },
+      {
+        path: "mobilabonnemang/vimla",
+        Component: VimlaMobileSeoPage,
+      },
+      {
+        path: "mobilabonnemang/fello",
+        Component: FelloMobileSeoPage,
+      },
+      {
+        path: "mobilabonnemang/tre",
+        Component: TreMobileSeoPage,
+      },
+      {
+        path: "mobilabonnemang/tele2",
+        Component: Tele2MobileSeoPage,
+      },
+      {
+        path: "mobilabonnemang/telenor",
+        Component: TelenorMobileSeoPage,
+      },
+      {
+        path: "mobilabonnemang/telia",
+        Component: TeliaMobileSeoPage,
+      },
+      {
+        path: "mobilabonnemang/utan-bindningstid",
+        Component: NoBindingMobileSeoPage,
+      },
+      {
+        path: "guider/hur-mycket-surf",
+        Component: SurfGuideMobileSeoPage,
       },
       {
         path: "elavtal",
