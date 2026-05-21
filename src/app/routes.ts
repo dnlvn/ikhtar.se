a1deefd8217d385f2bea3c4f2fe9d89f728d5865
dnlvn <dan@appero.se>
2026-05-21 20:39:41 +0200

    Add cheapest mobile SEO route

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router";
 import { Layout } from "@/app/components/Layout";
 import { LandingPage } from "@/app/pages/LandingPage";
 import { MobileComparison } from "@/app/pages/MobileComparison";
+import { CheapestMobileSubscriptionPage } from "@/app/pages/MobileSeoPage";
 import { ElectricityComparison } from "@/app/pages/ElectricityComparison";
 import { Integritetspolicy } from "@/app/pages/Integritetspolicy";
 import { Cookies } from "@/app/pages/Cookies";
@@ -18,6 +19,10 @@ export const router = createBrowserRouter([
       {
         path: "mobilabonnemang",
         Component: MobileComparison,
+      },
+      {
+        path: "mobilabonnemang/billigaste",
+        Component: CheapestMobileSubscriptionPage,
       },
       {
         path: "elavtal",
